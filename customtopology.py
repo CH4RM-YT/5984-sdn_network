2f37aa49510e639ac8c243d96bf6cbdc4fae9f28
Abiola-Ajayi Kehinde <106906689+CH4RM-YT@users.noreply.github.com>
2025-01-14 15:03:49 +0100

    Update customtopology.py

--- customtopology.py
+++ customtopology.py
@@ -16,17 +16,17 @@ def custom_tree_topology():
     )
 
     # Add core switch
-    core_switch = net.addSwitch('s1')
+    core_switch = net.addSwitch('switch1')
 
     # Add aggregation layer switches
-    agg_switch1 = net.addSwitch('s2')
-    agg_switch2 = net.addSwitch('s3')
+    agg_switch1 = net.addSwitch('switch2')
+    agg_switch2 = net.addSwitch('switch3')
 
     # Add access layer switches
-    access_switch1 = net.addSwitch('s4')
-    access_switch2 = net.addSwitch('s5')
-    access_switch3 = net.addSwitch('s6')
-    access_switch4 = net.addSwitch('s7')
+    access_switch1 = net.addSwitch('switch4')
+    access_switch2 = net.addSwitch('switch5')
+    access_switch3 = net.addSwitch('switch6')
+    access_switch4 = net.addSwitch('switch7')
 
     # Interconnect core and aggregation switches
     net.addLink(core_switch, agg_switch1, bw=50, delay='2ms', use_htb=True)
@@ -39,13 +39,13 @@ def custom_tree_topology():
     net.addLink(agg_switch2, access_switch4, bw=30, delay='5ms', use_htb=True)
 
     # Add hosts to access layer switches
-    host1 = net.addHost('h1', ip='10.0.0.1/24')
-    host2 = net.addHost('h2', ip='10.0.0.2/24')
-    host3 = net.addHost('h3', ip='10.0.0.3/24')
-    host4 = net.addHost('h4', ip='10.0.0.4/24')
-    host5 = net.addHost('h5', ip='10.0.1.1/24')
-    host6 = net.addHost('h6', ip='10.0.1.2/24')
-    host7 = net.addHost('h7', ip='10.0.1.3/24')
+    host1 = net.addHost('h1', ip='10.0.0.5/24')
+    host2 = net.addHost('h2', ip='10.0.0.3/24')
+    host3 = net.addHost('h3', ip='10.0.0.7/24')
+    host4 = net.addHost('h4', ip='10.0.0.9/24')
+    host5 = net.addHost('h5', ip='10.0.1.0/24')
+    host6 = net.addHost('h6', ip='10.0.1.6/24')
+    host7 = net.addHost('h7', ip='10.0.1.2/24')
     host8 = net.addHost('h8', ip='10.0.1.4/24')
 
     # Connect hosts to access layer switches
